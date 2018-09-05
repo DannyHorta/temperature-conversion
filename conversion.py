83c4947c7e06c605148a0d3ae50a836aa2479720
Nabil Brice <nabilbrice@yahoo.co.uk>
2018-09-05 11:38:51 +0100

    Add documentation to functions

--- conversion.py
+++ conversion.py
@@ -1,7 +1,13 @@
 def kelvin_to_celsius(temp):
+    """
+    Convert a temperature from Kelvin to Celsius
+    """
     return temp - 273.15
 
 def celsius_to_fahr(temp):
+    """
+    Convert a temperature from Celsius to Fahrenheit via Celsius
+    """
     return temp*(9/5)+32
 
 def kelvin_to_fahr(temp):
